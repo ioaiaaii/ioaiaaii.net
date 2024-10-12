962c295984d1b77e9f0413753278407cad6d0ba4
ioaiaaii <johnsavvaidis@gmail.com>
2024-10-12 11:04:11 +0300

    feat(data): Migrate to embed native go FS
    - data will be baked inside bin
    - cleaner conf

--- cmd/ioaiaaii.net/main.go
+++ cmd/ioaiaaii.net/main.go
@@ -4,26 +4,28 @@ import (
 	"log"
 	"time"
 
-	"ioaiaaii.net/config"
 	"ioaiaaii.net/internal/controller/httpcontroller"
 	"ioaiaaii.net/internal/infrastructure/cache"
-	"ioaiaaii.net/internal/infrastructure/persistence/file"
+	"ioaiaaii.net/internal/infrastructure/persistence/data"
+	"ioaiaaii.net/internal/infrastructure/persistence/storage"
 	"ioaiaaii.net/internal/infrastructure/transport/httpserver"
 	"ioaiaaii.net/internal/usecase/content"
 )
 
 func main() {
 
-	conf, err := config.LoadConfig()
+	var dataConf data.Config
 
+	err := dataConf.LoadData()
 	if err != nil {
-		log.Fatalf("Failed to load configuration: %v", err)
+		log.Fatalf("Error loading data: %v", err)
 	}
 
 	// 2. Initialize the repository (file-based repository in this case)
-	fileRepo := file.NewFileContent(conf.ResumeFile, conf.ReleasesFile, conf.LiveFile, conf.ProjectsFile)
+	fetchedData := storage.NewFileContent(dataConf)
+
 	// Add in-memory caching layer with a 10-minute TTL
-	cachedRepo := cache.NewInMemoryCacheRepository(fileRepo, 30*time.Minute)
+	cachedRepo := cache.NewInMemoryCacheRepository(fetchedData, 30*time.Minute)
 
 	// 3. Initialize the use case by injecting the repository
 	contentUsecase := content.NewContentUsecase(cachedRepo)
