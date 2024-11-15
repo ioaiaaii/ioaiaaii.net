caf97334d47a2bb54fa600f8355314b09356c4ce
ioaiaaii <johnsavvaidis@gmail.com>
2024-11-15 16:07:24 +0200

    chore(iac): module update with new images

--- deploy/iac/main.tf
+++ deploy/iac/main.tf
@@ -16,14 +16,14 @@ locals {
       predefined_acl = "publicRead"
     },
     dec_pdc_front = {
-      name         = "assets/images/releases/dec-pdc/front_v2.jpg"
-      source       = "${local.base_path}/assets/images/releases/dec-pdc/front_v2.jpg"
+      name         = "assets/images/releases/dec-pdc/front_v3.jpg"
+      source       = "${local.base_path}/assets/images/releases/dec-pdc/front_v3.jpg"
       cache_control = "public, max-age=2592000"
       content_type = "image/jpeg"
     },
     dec_pdc_record = {
       name         = "assets/images/releases/dec-pdc/render.jpg"
-      source       = "${local.base_path}/assets/images/releases/dec-pdc/render.jpg"
+      source       = "${local.base_path}/assets/images/releases/dec-pdc/Mockup.jpg"
       cache_control = "public, max-age=2592000"
       content_type = "image/jpeg"
     },
@@ -40,14 +40,14 @@ locals {
       content_type = "image/jpeg"
     },
     diataxis_render_a = {
-      name         = "assets/images/releases/diataxis/render_a.jpg"
-      source       = "${local.base_path}/assets/images/releases/diataxis/render_a.jpg"
+      name         = "assets/images/releases/diataxis/mockup_a_v2.jpg"
+      source       = "${local.base_path}/assets/images/releases/diataxis/mockup_a_v2.jpg"
       cache_control = "public, max-age=2592000"
       content_type = "image/jpeg"
     },
     diataxis_render_b = {
-      name         = "assets/images/releases/diataxis/render_b.jpg"
-      source       = "${local.base_path}/assets/images/releases/diataxis/render_b.jpg"
+      name         = "assets/images/releases/diataxis/mockup_b_v2.jpg"
+      source       = "${local.base_path}/assets/images/releases/diataxis/mockup_b_v2.jpg"
       cache_control = "public, max-age=2592000"
       content_type = "image/jpeg"
     },
