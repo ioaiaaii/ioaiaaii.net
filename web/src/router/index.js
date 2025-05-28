ee417645bc389875725a8525b28befdfbd765fc8
ioaiaaii <johnsavvaidis@gmail.com>
2025-05-28 14:50:54 +0300

    feat(release): deprecate release page in favor of artist info

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -2,7 +2,6 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from '@/components/Home.vue';
 import Info from '@/components/Info.vue';
 import Projects from '@/components/Projects.vue';
-import Releases from '@/components/Releases.vue';
 import Live from '@/components/Live.vue';
 import NotFound from '@/components/NotFound.vue';
 import Contact from '@/components/Contact.vue';
@@ -12,7 +11,6 @@ const routes = [
   { path: '/', name: 'Home', component: Home },
   { path: '/info', name: 'Info', component: Info },
   { path: '/projects', name: 'Projects', component: Projects },
-  { path: '/releases', name: 'Releases', component: Releases },
   { path: '/live', name: 'Live', component: Live },
   { path: '/contact', name: 'Contact', component: Contact },
   { path: '/cv', name: 'CV', component: CV },
